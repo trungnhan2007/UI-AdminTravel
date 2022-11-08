45dd60bd6444f176d538c859656cf6a471a0ea18
trungnhan <lonelytoo2007@gmail.com>
2022-11-08 21:50:01 +0700

    update

--- src/jsx/components/table/EnterpriseTable/Columns.js
+++ src/jsx/components/table/EnterpriseTable/Columns.js
@@ -1,108 +1,107 @@
-import { ColumnFilter } from './ColumnFilter';
+import { ColumnFilter } from "./ColumnFilter";
 
 export const COLUMNS = [
+  {
+    Header: "#",
+    Footer: "#",
+    accessor: "enId",
+    Filter: ColumnFilter,
+    //disableFilters: true,
+  },
 
-	{
-		Header : '#',
-		Footer : '#',
-		accessor: 'en_id',
-		Filter: ColumnFilter,
-		//disableFilters: true,
-	},
-	
-	{
-		Header : 'Name',
-		Footer : 'Name',
-		accessor: 'en_namecompany',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Email Id',
-		Footer : 'Email Id',
-		accessor: 'en_email',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Phone',
-		Footer : 'Phone',
-		accessor: 'en_phone',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Phone',
-		Footer : 'Phone',
-		accessor: 'en_code',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Phone',
-		Footer : 'Phone',
-		accessor: 'en_address',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Username Id',
-		Footer : 'Username Id',
-		accessor: 'en_username',
-		Filter: ColumnFilter,
-	},
-	
-	{
-		Header : 'Avatar',
-		Footer : 'Avatar',
-		accessor: 'en_avatar',
-		Filter: ColumnFilter,
-	},
+  {
+    Header: "Name",
+    Footer: "Name",
+    accessor: "enNamecompany",
+    Filter: ColumnFilter,
+  },
+  {
+    Header: "Email Id",
+    Footer: "Email Id",
+    accessor: "enEmail",
+    Filter: ColumnFilter,
+  },
+  {
+    Header: "Phone",
+    Footer: "Phone",
+    accessor: "enPhone",
+    Filter: ColumnFilter,
+  },
+  {
+    Header: "Phone",
+    Footer: "Postalcode",
+    accessor: "enPostalcode",
+    Filter: ColumnFilter,
+  },
+  {
+    Header: "Address",
+    Footer: "Address",
+    accessor: "enAddress",
+    Filter: ColumnFilter,
+  },
+  {
+    Header: "District",
+    Footer: "District",
+    accessor: "enDistrict",
+    Filter: ColumnFilter,
+  },
 
-	{
-		Header : 'Actions',
-		Footer : 'Actions',
-		accessor: '',
-		Filter: ColumnFilter,
-	},
-]
+  {
+    Header: "Country",
+    Footer: "Country",
+    accessor: "enCountry",
+    Filter: ColumnFilter,
+  },
+
+  {
+    Header: "Actions",
+    Footer: "Actions",
+    accessor: "",
+    Filter: ColumnFilter,
+  },
+];
 
 export const GROUPED_COLUMNS = [
-	{
-		Header : 'Id',
-		Footer : 'Id',
-		accessor: 'id'
-	},
-	{
-		Header : 'Name',
-		Footer : 'Name',
-		columns: [
-			{
-				Header : 'First Name',
-				Footer : 'First Name',
-				accessor: 'first_name'
-			},
-			{
-				Header : 'Last Name',
-				Footer : 'Last Name',
-				accessor: 'last_name'
-			},
-		]
-	},
-	{
-		Header: 'Info',
-		Footer: 'Info',
-		columns: [
-			{
-				Header : 'Date of  Birth',
-				Footer : 'Date of  Birth',
-				accessor: 'date_of_birth'
-			},
-			{
-				Header : 'Country',
-				Footer : 'Country',
-				accessor: 'country',
-			},
-			{
-				Header : 'Phone',
-				Footer : 'Phone',
-				accessor: 'phone'
-			},
-		]
-	},
-]
+  {
+    Header: "Id",
+    Footer: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        Footer: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        Footer: "Last Name",
+        accessor: "last_name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Date of  Birth",
+        Footer: "Date of  Birth",
+        accessor: "date_of_birth",
+      },
+      {
+        Header: "Country",
+        Footer: "Country",
+        accessor: "country",
+      },
+      {
+        Header: "Phone",
+        Footer: "Phone",
+        accessor: "phone",
+      },
+    ],
+  },
+];
--- src/jsx/components/table/FilteringTable/Columns.js
+++ src/jsx/components/table/FilteringTable/Columns.js
@@ -1,96 +1,75 @@
-import { ColumnFilter } from './ColumnFilter';
+import { ColumnFilter } from "./ColumnFilter";
+import Button from "react-bootstrap/Button";
 
 export const COLUMNS = [
+  {
+    Header: "#",
+    Footer: "#",
+    accessor: "user_id",
+    Filter: ColumnFilter,
+    disableFilters: true,
+  },
 
-	{
-		Header : '#',
-		Footer : '#',
-		accessor: 'user_id',
-		Filter: ColumnFilter,
-		//disableFilters: true,
-	},
-	
-	{
-		Header : 'Name',
-		Footer : 'Name',
-		accessor: 'user_name',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Email Id',
-		Footer : 'Email Id',
-		accessor: 'user_email',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Phone',
-		Footer : 'Phone',
-		accessor: 'user_phone',
-		Filter: ColumnFilter,
-	},
-	{
-		Header : 'Username Id',
-		Footer : 'Username Id',
-		accessor: 'user_username',
-		Filter: ColumnFilter,
-	},
-	
-	{
-		Header : 'Avatar',
-		Footer : 'Avatar',
-		accessor: 'user_avatar',
-		Filter: ColumnFilter,
-	},
+  {
+    Header: "Name",
+    Footer: "Name",
+    accessor: "user_name",
+    Filter: ColumnFilter,
+    disableFilters: true,
+  },
+  {
+    Header: "Email Id",
+    Footer: "Email Id",
+    accessor: "user_email",
+    Filter: ColumnFilter,
+    disableFilters: true,
+  },
+  {
+    Header: "Phone",
+    Footer: "Phone",
+    accessor: "user_phone",
+    Filter: ColumnFilter,
+    disableFilters: true,
+  },
+  {
+    Header: "Username Id",
+    Footer: "Username Id",
+    accessor: "user_username",
+    Filter: ColumnFilter,
+    disableFilters: true,
+  },
 
-	{
-		Header : 'Actions',
-		Footer : 'Actions',
-		accessor: '',
-		Filter: ColumnFilter,
-	},
-]
+  {
+    Header: "Avatar",
+    Footer: "Avatar",
+    accessor: "user_avatar",
+    Filter: ColumnFilter,
+    disableFilters: true,
+  },
 
-export const GROUPED_COLUMNS = [
-	{
-		Header : 'Id',
-		Footer : 'Id',
-		accessor: 'id'
-	},
-	{
-		Header : 'Name',
-		Footer : 'Name',
-		columns: [
-			{
-				Header : 'First Name',
-				Footer : 'First Name',
-				accessor: 'first_name'
-			},
-			{
-				Header : 'Last Name',
-				Footer : 'Last Name',
-				accessor: 'last_name'
-			},
-		]
-	},
-	{
-		Header: 'Info',
-		Footer: 'Info',
-		columns: [
-			{
-				Header : 'Date of  Birth',
-				Footer : 'Date of  Birth',
-				accessor: 'date_of_birth'
-			},
-			{
-				Header : 'Country',
-				Footer : 'Country',
-				accessor: 'country',
-			},
-			{
-				Header : 'Phone',
-				Footer : 'Phone',
-				accessor: 'phone'
-			},
-		]
-	},
-]
+  {
+    Header: "Edit",
+    Footer: "Edit",
+    accessor: "",
+    Cell: (
+      <Button size="sm" variant="outline-primary">
+        Edit
+      </Button>
+    ),
+    Filter: ColumnFilter,
+    disableFilters: true,
+  },
+  {
+    Header: "Delete",
+    Footer: "Delete",
+    accessor: "",
+    Filter: ColumnFilter,
+    disableFilters: true,
+
+    Cell: (
+      <Button size="sm" variant="outline-danger">
+        Delete
+      </Button>
+    ),
+  },
+];
